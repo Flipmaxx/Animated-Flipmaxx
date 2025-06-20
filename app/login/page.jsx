52498b1aa8fd5bce7ee4a59ed55f9a889135b207
nidinbose <nidinbose700@gmail.com>
2025-06-20 10:28:43 +0530

    adding

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -45,11 +45,33 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black text-white">
-      <form
+<div className='bg-black'>
+
+      <div className="min-h-screen max-w-2xl mx-auto my-auto bg-black text-white">
+      <div className='grid grid-cols-1 gap-10 max-w-full p-5 md:p-12'>
+        <div className="w-full flex flex-col items-center md:items-center justify-center gap-4 text-center md:text-left  rounded-xl">
+  <img
+    src="./Images/LOGOWHITE.png"
+    alt="Logo"
+    className="max-w-auto h-auto"
+  />
+  <h1 className="text-white font-bold text-xl sm:text-3xl md:text-4xl lg:text-3xl leading-tight">
+    We are the best in business
+  </h1>
+  <p className='text-center'>we are service provider that helps businesses transform through design and marketing strategies, likely with an international presence.</p>
+</div>
+
+            <form
         onSubmit={handleSubmit}
-        className="bg-white text-black p-8 rounded-lg w-full max-w-md shadow-xl"
+        className="bg-white text-black p-8 rounded-lg w-full max-w-full shadow-xl"
       >
+        <div className='flex items-center justify-center mx-auto max-w-sm'>
+            <img src="./Images/Flip.png" alt="" />
+            
+             {/* <h2 className="text-2xl font-bold mb-6 text-center">Login</h2> */}
+        </div>
+
+
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
 
         {error && <p className="mb-4 text-red-600 text-sm text-center">{error}</p>}
@@ -86,6 +108,8 @@ export default function LoginPage() {
           {loading ? 'Logging in...' : 'Login'}
         </button>
       </form>
+      </div>
     </div>
+</div>
   );
 }
