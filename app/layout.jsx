fb62803b8ff41eb662b491961b888ec55a9bfbcd
Flipmaxx <flipmaxxglobal@gmail.com>
2025-07-26 12:32:07 +0530

    addin

--- app/layout.jsx
+++ app/layout.jsx
@@ -27,16 +27,17 @@ export default function RootLayout({ children }) {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '631315399412741');
-              fbq('track', 'PageView');
+            !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1254259836485598');
+fbq('track', 'PageView');
+
             `,
           }}
         />
@@ -50,7 +51,7 @@ export default function RootLayout({ children }) {
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=631315399412741&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1254259836485598&ev=PageView&noscript=1"
             alt="Meta Pixel"
           />
         </noscript>
